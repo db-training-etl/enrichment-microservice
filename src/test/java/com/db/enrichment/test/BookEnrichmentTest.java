832529ec6567ad642e90fbe732fef84d9950f434
Samyr <105075171+sakhmed99@users.noreply.github.com>
2022-10-17 15:30:16 +0200

    Added Entity and Starter Tests (#5)
    * Added Entity and packages
    
    * Add starter Tests
    
    Co-authored-by: Samyr Ahmad Sapsalyov <samyr.ahmad@gft.com>

---
+++ src/test/java/com/db/enrichment/test/BookEnrichmentTest.java
@@ -0,0 +1,31 @@
+package com.db.enrichment.test;
+
+
+import com.db.enrichment.config.BookEnrichment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class BookEnrichmentTest {
+
+    private BookEnrichment underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new BookEnrichment();
+    }
+
+    @Test
+    void itShould() {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
+
+
+
+
